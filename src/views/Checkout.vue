4c5efe4df8b51d25229dc8f14de35f9464ceba63
sukram123 <sukram123@haidt.com>
2019-01-31 23:16:57 +0100

    Updated Checkout.vue

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -1,6 +1,27 @@
 <template>
     <v-app>
-        <Header/>
+        <v-toolbar app>
+            <v-toolbar-title class="headline text-uppercase">
+                <img src="https://www.5degeneve.ch/5deGeneve.png" alt="5 de Geneve" width=230 height=45 />
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+                <span style="font-style: italic; font-size: 25px;">PRIVATE CRUISE GENEVA LAKE</span>
+            <v-spacer></v-spacer>
+            <v-btn 
+                v-for="lang in $ml.list"
+                :key="lang"
+                @click="change_lang(lang,$ml)"
+                flat 
+                icon 
+            >
+                <img :src="require(`@/assets/${lang}.png`)" :alt="lang" height=35 />
+            </v-btn>
+
+            <RegisterAgency v-if="$cookies.isKey('token') === false"/>
+            <v-btn v-on:click="logout" round color="error" v-if="$cookies.isKey('token') === true">
+                <span class="mr-2">Logout</span>
+            </v-btn>          
+        </v-toolbar>
 
         <v-content style="padding-bottom:100px;">
             <v-layout justify-center justify-space-around style="flex-basis: 0; flex-grow: 1;">
@@ -23,8 +44,8 @@
                 <v-flex xs6>
                     The Total Price of your Cruise is {{ order.price }} CHF. A minimum down-payment of 30% is expected to be payed within 10 days after reservation. But you may choose to pay the full amount right away. Please note that if you choose the 30% option, you will have to pay the remaining 70% latest 30 days before departure or your Cruise will be canceled.
                     <v-radio-group v-model="radioGroup">
-                        <v-radio value='full' :label="priceFullLabel()"></v-radio>
-                        <v-radio value='down' :label="priceDownLabel()"></v-radio>
+                        <v-radio value='full' :label="prices.fullLabel"></v-radio>
+                        <v-radio value='down' :label="prices.downLabel"></v-radio>
                     </v-radio-group>                    
                     <PayPal
                         :amount="pay_price"
@@ -48,7 +69,7 @@
 <script>
     import { MLBuilder } from 'vue-multilanguage';
     import axios from 'axios';
-    import Header from '../components/Header';
+    import RegisterAgency from '../components/RegisterAgency';
     import Footer from '../components/Footer';
     import Product from '../components/Product';
     import PayPal from 'vue-paypal-checkout';
@@ -56,7 +77,7 @@
     export default {
         name: 'App',
         components: {
-            Header,
+            RegisterAgency,
             Footer,
             Product,
             PayPal
@@ -66,14 +87,6 @@
                 this.$cookies.remove('token');
                 this.$cookies.remove('role');
                 location.reload();
-            },
-            priceFullLabel() {
-                this.pay_price = this.order.price;
-                return this.order.price + " CHF .   [the full amount - no further payment required]";
-            },
-            priceDownLabel() {
-                this.pay_price = Math.round(this.order.price * 3 / 10);
-                return Math.round(this.order.price * 3 / 10) + " CHF .   [the 30% down-payment]";
             },
             change_lang: function(lang,$ml){ 
                 $ml.change(lang);               
@@ -88,6 +101,10 @@
             axios.get('https://www.5degeneve.ch/api/order?id='+this.$data.order.orderID)
                 .then((response) => {
                     this.order = response.data[0];
+                    this.prices.full      = this.order.price;
+                    this.prices.fullLabel = this.prices.full + " CHF .   [the full amount - no further payment required]";
+                    this.prices.down      = Math.round(this.order.price * 3 / 10);
+                    this.prices.downLabel = this.prices.down + " CHF .   [the 30% down-payment]";
                 })
                 .catch(error => console.log(error));
 
@@ -118,6 +135,12 @@
                     price: 1000,
                     caption: '',
                     type: ''
+                },
+                prices: {
+                    full: 0,
+                    fullLabel: '',
+                    down: 0,
+                    downLabel: ''
                 }
             }
         }
